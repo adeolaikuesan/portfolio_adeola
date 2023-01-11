3c421cd2f9f8f0e78ba1e71324c437a19a0b8a83
@max.naumanen <max.naumanen@tbwa.fi>
2023-01-11 19:31:50 +0200

    tein etusivun sillei mobiili thing myös

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home";
 import Projects from "./pages/projects";
 import About from "./pages/about";
 import { Route, Routes } from "react-router-dom";
+import "./styles.scss"
 
 function App() {
   return (
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,6 @@ import {
   FaSpotify,
 } from "react-icons/fa";
 // import "../App.css";
-import "../styles.scss";
 
 // import Container from "react-bootstrap/Container";
 // import Row from "react-bootstrap/Row";
@@ -22,28 +21,30 @@ function Home() {
         /> */}
       </div>
       <div className="section-2">
-        <h1>
-          Adeola <br /> Ikuesan
-        </h1>
-        <p className="homeDescription">
-          Musician and software developer
-        </p>
-        <div className="icons">
-          <a href="mailto: adeola.ikuesan@gmail.com">
-            <FaEnvelope className="icon" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/adeola-ikuesan-227b89ab/"
-            target="_blank"
-          >
-            <FaLinkedinIn className="icon" />
-          </a>
-          <a
-            href="https://github.com/adeolaikuesan"
-            target="_blank"
-          >
-            <FaGithub className="icon" />
-          </a>
+        <div className="wrapper">
+          <h1>
+            Adeola <br /> Ikuesan
+          </h1>
+          <p className="homeDescription">
+            Musician and software developer
+          </p>
+          <div className="icons">
+            <a href="mailto: adeola.ikuesan@gmail.com">
+              <FaEnvelope className="icon" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/adeola-ikuesan-227b89ab/"
+              target="_blank"
+            >
+              <FaLinkedinIn className="icon" />
+            </a>
+            <a
+              href="https://github.com/adeolaikuesan"
+              target="_blank"
+            >
+              <FaGithub className="icon" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
